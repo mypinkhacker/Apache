f0c8205379703f54f4fa705e2daadc92b9672e9a
mypinkhacker <51525807+mypinkhacker@users.noreply.github.com>
2019-10-27 21:44:00 +0700

    fix: Correct a minor typo in documentation on main.php

---
+++ main.php
@@ -0,0 +1,24 @@
+<?php
+class Project {
+    public $name;
+    public $version;
+
+    public function __construct($name, $version) {
+        $this->name = $name;
+        $this->version = $version;
+    }
+
+    public function display() {
+        echo "Project: {$this->name}, Version: {$this->version}\n";
+    }
+}
+
+$p = new Project("GitHub Automated Repository Project", "1.0.0");
+$p->display();
+echo "\nFeatures: Classes, Constructors, Loops\n";
+for ($i = 0; $i < 5; $i++) {
+    echo "  - Iteration " . ($i + 1) . "\n";
+}
+?>
+
+# Modified on 2019-10-27T00:00:00
